fc396c5b2ca2662854018fb8d8c4b604225d99ef
avisek123 <sahooavisek41@gmail.com>
2023-03-01 00:54:07 +0530

    debounce done

--- App.tsx
+++ App.tsx
@@ -1,15 +1,74 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import ChatDelete from './animation/chatDelete/ChatDelete';
+import React, {useState, useEffect} from 'react';
+import {TextInput, FlatList, Text, View} from 'react-native';
 
-const App = () => {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <ChatDelete />
-    </SafeAreaView>
-  );
+const useDebounce = (value: any, delay: any) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
 };
 
-export default App;
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-const styles = StyleSheet.create({});
+  const debouncedSearchTerm = useDebounce(searchTerm, 2000);
+
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      fetchSearchResults();
+    } else {
+      setSearchResults([]);
+    }
+  }, [debouncedSearchTerm]);
+  console.log('debouncedSearchTerm', debouncedSearchTerm);
+  const fetchSearchResults = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://demo.dataverse.org/api/search?q=${debouncedSearchTerm}`,
+      );
+      const data = await response.json();
+      console.log('dat', data?.results);
+      setSearchResults(data.results);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const renderItem = ({item}: any) => <Text>{item.title}</Text>;
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginTop: 90,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <TextInput
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+        placeholder="Search"
+        onSubmitEditing={fetchSearchResults}
+      />
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList data={searchResults} renderItem={renderItem} />
+      )}
+    </View>
+  );
+};
+export default App;
